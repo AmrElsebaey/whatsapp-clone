25cd9bebc1c1e30bd218c9c78914d6c883c0c486
Amr Elsebay <amrelsebay3@gmail.com>
2025-09-02 15:40:30 +0300

    Implement the chat list component

--- whatsapp-clone-ui/src/app/components/chat-list/chat-list.ts
+++ whatsapp-clone-ui/src/app/components/chat-list/chat-list.ts
@@ -1,14 +1,48 @@
 import { Component, input, InputSignal } from '@angular/core';
-import { ChatResponse } from '../../services/models';
+import {ChatResponse, UserResponse} from '../../services/models';
+import {DatePipe} from '@angular/common';
+import {UserService} from '../../services/services/user.service';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './chat-list.html',
   styleUrl: './chat-list.scss'
 })
 export class ChatList {
 
   chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
+  searchNewContact = false;
+  contacts: Array<UserResponse> = [];
+
+  constructor(
+    private userService: UserService
+  ) {
+  }
+
+  searchContact() {
+    this.userService.getAllUsers()
+      .subscribe( {
+        next: (users) => {
+          this.contacts = users;
+          this.searchNewContact = true;
+        }
+      })
+  }
+
+  chatClicked(chat: ChatResponse) {
+  }
+
+  wrapMessage(lastMessage: string | undefined):string {
+    if (lastMessage && lastMessage.length <= 20) {
+      return lastMessage;
+    }
+    return lastMessage?.substring(0, 17) + '...';
+  }
+
+  selectContact(contact: UserResponse) {
+  }
 
 }
--- whatsapp-clone-ui/src/app/pages/main/main.ts
+++ whatsapp-clone-ui/src/app/pages/main/main.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChatResponse } from '../../services/models';
 import { ChatService } from '../../services/services';
 import { ChatList } from '../../components/chat-list/chat-list';
+import {KeycloakService} from '../../utils/keycloak/keycloak';
 
 @Component({
   selector: 'app-main',
@@ -14,7 +15,8 @@ export class Main implements OnInit{
   chats: Array<ChatResponse> = [];
 
   constructor(
-    private chatService: ChatService
+    private chatService: ChatService,
+    private keycloakService: KeycloakService
   ) { }
 
   ngOnInit(): void {
@@ -29,7 +31,15 @@ export class Main implements OnInit{
       },
       error: (err) => {
         console.error('Error fetching chats:', err);
-      } 
+      }
     })
+  }
+
+  logout() {
+    this.keycloakService.logout();
+  }
+
+  userProfile() {
+    this.keycloakService.accountManagement();
   }
 }
